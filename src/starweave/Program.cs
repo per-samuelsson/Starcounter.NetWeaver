ab9b3f44fbd603805a45e369a511599be5d6a7c8
Per Samuelsson <per@starcounter.com>
2017-10-02 14:31:24 +0200

    Changed starweave to a .NET Core 2.0 app.

--- src/starweave/Program.cs
+++ src/starweave/Program.cs
@@ -1,15 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Starcounter.Weaver;
+﻿using Starcounter.Weaver;
+using System;
 
-namespace starweave {
-    class Program {
-        static void Main(string[] args) {
-            var weaver = new AssemblyWeaver(System.IO.Path.GetFullPath(@"..\..\..\AppNet\bin\debug\AppNet.exe"));
-            weaver.Weave();
+namespace starweave
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length == 0) {
+                Console.WriteLine("Usage: starweave <assembly>");
+                return 1;
+            }
+
+            var w = new AssemblyWeaver(args[0]);
+            w.Weave();
+
+            return 0;
         }
     }
 }
