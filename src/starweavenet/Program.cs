3a23424b38ba5f67458d985dc0810338ec2a81ea
Per Samuelsson <per@starcounter.com>
2017-10-06 14:44:26 +0200

    Added specific .NET Framework weaver driver, assuring we can weave on all environments.

---
+++ src/starweavenet/Program.cs
@@ -0,0 +1,27 @@
+﻿using Starcounter.Weaver;
+using System;
+using System.IO;
+
+namespace starweavenet {
+    class Program {
+        static int Main(string[] args) {
+            if (args.Length == 0) {
+                Console.WriteLine("Usage: starweavenet <assembly>");
+                return 1;
+            }
+
+            var dir = Path.GetDirectoryName(args[0]);
+            dir = Path.Combine(dir, ".starcounter");
+            if (!Directory.Exists(dir)) {
+                Directory.CreateDirectory(dir);
+            }
+
+            var w = new AssemblyWeaver(args[0], dir);
+            w.Weave();
+
+            Console.WriteLine($"Weaved {w.AssemblyPath} -> {w.WeavedAssemblyPath}");
+
+            return 0;
+        }
+    }
+}
