3312b36879465add819ad57fa2c5485d61a8965c
Per Samuelsson <per@starcounter.com>
2017-10-09 13:04:45 +0200

    Changed CRUD facade prototype to resemble live one 1-1

--- src/Starcounter2/Internal/WeaverFacade/CRUD.cs
+++ src/Starcounter2/Internal/WeaverFacade/CRUD.cs
@@ -3,15 +3,26 @@
 namespace Starcounter2.Internal.WeaverFacade {
 
     public static class CRUD {
-        public static void Insert(ulong createHandle, out ulong dbId) {
+
+        public static void CreateCrud(out ulong dbId, out ulong dbRef, ulong crudHandle) {
             dbId = 42;
+            dbRef = 42;
         }
 
-        public static string ReadString(ulong dbId, ulong readHandle) {
-            return "42";
+        public static int GetInt(ulong dbId, ulong dbRef, ulong crudHandle) {
+            return 42;
         }
 
-        public static void WriteString(ulong dbId, ulong writeHandle, string value) {
+        public static void SetInt(ulong dbId, ulong dbRef, ulong crudHandle, int value) {
+
+        }
+
+        public static int? GetNullableInt(ulong dbId, ulong dbRef, ulong crudHandle) {
+            return null;
+        }
+
+        public static void SetNullableInt(ulong dbId, ulong dbRef, ulong crudHandle, int? value) {
+
         }
     }
 }
