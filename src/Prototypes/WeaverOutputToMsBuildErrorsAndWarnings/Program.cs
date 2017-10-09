4979fead7c240ff036f1941c14101bf3c3c6b253
Per Samuelsson <per@starcounter.com>
2017-10-09 13:48:49 +0200

    Disable prototypes emitting errors for now.

--- src/Prototypes/WeaverOutputToMsBuildErrorsAndWarnings/Program.cs
+++ src/Prototypes/WeaverOutputToMsBuildErrorsAndWarnings/Program.cs
@@ -16,8 +16,8 @@ namespace WeaverOutputToMsBuildErrorsAndWarnings {
     class Program {
         static void Main(string[] args) {
             Console.Error.WriteLine($"{Assembly.GetExecutingAssembly().Location}: warning CS9999: This is a warning");
-            Console.Error.WriteLine($"{Assembly.GetExecutingAssembly().Location}: error CS9999: This is an error");
-            Environment.ExitCode = 1;
+            // Console.Error.WriteLine($"{Assembly.GetExecutingAssembly().Location}: error CS9999: This is an error");
+            // Environment.ExitCode = 1;
         }
     }
 }
