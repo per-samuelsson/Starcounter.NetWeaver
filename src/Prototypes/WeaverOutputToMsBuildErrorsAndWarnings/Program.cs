7bf4fd243f44b755c4e5a00f7b5505b47eab0737
Per Samuelsson <per@starcounter.com>
2017-10-09 09:27:18 +0200

    Two prototype projects showing how to invoke weaver and properly generate warnings and errors.

---
+++ src/Prototypes/WeaverOutputToMsBuildErrorsAndWarnings/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.Reflection;
+
+namespace WeaverOutputToMsBuildErrorsAndWarnings {
+
+    // After this project build, as part of AfterBuild, we'll run the exe and capture
+    // its output, producing MsBuild errors and warnings from it.
+
+    // Using this format:
+    // https://github.com/Microsoft/msbuild/blob/master/src/Shared/CanonicalError.cs
+    //
+    // Some background:
+    // http://blogs.msdn.com/b/msbuild/archive/2006/11/03/msbuild-visual-studio-aware-error-messages-and-message-formats.aspx
+
+    class Program {
+        static void Main(string[] args) {
+            Console.Error.WriteLine($"{Assembly.GetExecutingAssembly().Location}: warning CS9999: This is a warning");
+            Console.Error.WriteLine($"{Assembly.GetExecutingAssembly().Location}: error CS9999: This is an error");
+            Environment.ExitCode = 1;
+        }
+    }
+}
---
+++ src/Prototypes/WeaverOutputToMsBuildErrorsAndWarningsCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Reflection;
+
+namespace WeaverOutputToMsBuildErrorsAndWarningsCore {
+    class Program {
+        static void Main(string[] args) {
+            Console.Error.WriteLine($"{Assembly.GetExecutingAssembly().Location}: warning CS9999: This is a warning");
+            Console.Error.WriteLine($"{Assembly.GetExecutingAssembly().Location}: error CS9999: This is an error");
+            Environment.ExitCode = 1;
+        }
+    }
+}
