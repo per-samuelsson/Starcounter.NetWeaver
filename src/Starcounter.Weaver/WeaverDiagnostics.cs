7927cd4dca4335ece5098cdce2a571dbcd33e3b3
Per Samuelsson <per@starcounter.com>
2017-10-10 14:51:16 +0200

    New WeaverDiagnostics class, just a dummy to get started with.

---
+++ src/Starcounter.Weaver/WeaverDiagnostics.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Starcounter.Weaver {
+    /// <summary>
+    /// Just some basic diagnstics API to start from.
+    /// </summary>
+    public class WeaverDiagnostics {
+
+        public static WeaverDiagnostics Quiet {
+            get {
+                return new WeaverDiagnostics();
+            }
+        }
+
+        public virtual void WriteError(string msg) {
+
+        }
+
+        public virtual void WriteWarning(string msg) {
+
+        }
+
+        public virtual void Trace(string msg) {
+
+        }
+    }
+}
