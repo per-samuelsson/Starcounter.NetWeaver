356278261615398cb013e34d2adf4c4b198eb465
Per Samuelsson <per@starcounter.com>
2017-10-11 17:15:16 +0200

    New AnalysisExtensionMethods class with a stub method to merge schemas.

---
+++ src/Starcounter.Weaver/Analysis/AnalysisExtensionMethods.cs
@@ -0,0 +1,12 @@
+﻿
+using Starcounter.Hosting.Schema;
+using System;
+
+namespace Starcounter.Weaver.Analysis {
+
+    public static class AnalysisExtensionMethods {
+        public static DatabaseSchema MergeWith(this DatabaseSchema schema, DatabaseSchema other) {
+            throw new NotImplementedException();
+        }
+    }
+}
