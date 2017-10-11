f2e1bdaac137c89896b74db9f05792310d037a9b
Per Samuelsson <per@starcounter.com>
2017-10-11 09:28:15 +0200

    New test utility class: TestUtilities

---
+++ test/Starcounter.Weaver.Tests/TestUtilities.cs
@@ -0,0 +1,22 @@
+﻿using Mono.Cecil;
+using System.Reflection;
+using Xunit;
+
+using ModuleDefinition = Mono.Cecil.ModuleDefinition;
+
+namespace Starcounter.Weaver.Tests {
+    public static class TestUtilities {
+        static ModuleDefinition currentAssemblyModule;
+
+        public static ModuleDefinition GetModuleOfCurrentAssembly(ReaderParameters readerParameters = null) {
+            if (currentAssemblyModule == null) {
+                var currentAssemblyPath = Assembly.GetExecutingAssembly().Location;
+                readerParameters = readerParameters ?? new DefaultModuleReaderParameters(currentAssemblyPath).Parameters;
+                var module = ModuleDefinition.ReadModule(currentAssemblyPath, readerParameters);
+                Assert.NotNull(module);
+                currentAssemblyModule = module;
+            }
+            return currentAssemblyModule;
+        }
+    }
+}
