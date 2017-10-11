b9390f82c96d7c7c3079935c2415da12a205250b
Per Samuelsson <per@starcounter.com>
2017-10-11 11:44:08 +0200

    Some new test utilities

--- test/Starcounter.Weaver.Tests/TestUtilities.cs
+++ test/Starcounter.Weaver.Tests/TestUtilities.cs
@@ -1,4 +1,5 @@
 ﻿using Mono.Cecil;
+using Starcounter.Weaver.Analysis;
 using System.Reflection;
 using Xunit;
 
@@ -7,6 +8,28 @@ using ModuleDefinition = Mono.Cecil.ModuleDefinition;
 namespace Starcounter.Weaver.Tests {
     public static class TestUtilities {
         static ModuleDefinition currentAssemblyModule;
+        static WeaverDiagnostics quietDiagnostics;
+        static ModuleReferenceDiscovery adviceAllReferenceDiscovery;
+
+        public static WeaverDiagnostics QuietDiagnostics {
+            // Use this. We don't now if we want to keep WeaverDiagnostics.Quiet.
+            get {
+                if (quietDiagnostics == null) {
+                    quietDiagnostics = WeaverDiagnostics.Quiet;
+                }
+                return quietDiagnostics;
+            }
+        }
+
+        public static ModuleReferenceDiscovery AdviceAllReferenceDiscovery {
+            get {
+                if (adviceAllReferenceDiscovery == null) {
+                    var diag = TestUtilities.QuietDiagnostics;
+                    adviceAllReferenceDiscovery = new ModuleReferenceDiscovery(new AdviceAllAdvisor(diag), diag);
+                }
+                return adviceAllReferenceDiscovery;
+            }
+        }
 
         public static ModuleDefinition GetModuleOfCurrentAssembly(ReaderParameters readerParameters = null) {
             if (currentAssemblyModule == null) {
