9f0a92456589b52c00b8e2a70952e56c034c3107
Per Samuelsson <per@starcounter.com>
2017-10-11 17:16:39 +0200

    New set of classes implementing pre analysis of an assembly.

---
+++ src/Starcounter.Weaver/Analysis/DefaultPreAnalysis.cs
@@ -0,0 +1,29 @@
+﻿
+using Mono.Cecil;
+using Starcounter.Hosting.Schema;
+
+namespace Starcounter.Weaver.Analysis {
+
+    public class DefaultPreAnalysis : PreAnalysis {
+
+        // Capture Starcounter module and all modules referenced that contains
+        // a schema. These modules are those that interest us.
+        //
+        // Ideally, we also want to provide a warning for any reference that
+        //   1. dont contain a schema, AND
+        //   2. still reference Starcounter
+        // That could be an indication it's not weaved, but should have been.
+
+        public DefaultPreAnalysis() : base() {
+        }
+
+        protected override bool IsTargetModule(ModuleDefinition candidate) {
+            return candidate.Name.Equals("Starcounter2.dll");
+        }
+
+        protected override DatabaseSchema DiscoverSchema(ModuleDefinition candidate, ISchemaSerializer serializer) {
+            var embeddedSchemaData = candidate.ReadEmbeddedResource(ModuleAnalyzer.SchemaResourceName);
+            return embeddedSchemaData != null ? serializer.Deserialize(embeddedSchemaData) : null;
+        }
+    }
+}
---
+++ src/Starcounter.Weaver/Analysis/PreAnalysis.cs
@@ -0,0 +1,52 @@
+﻿using Mono.Cecil;
+using Starcounter.Hosting.Schema;
+
+namespace Starcounter.Weaver.Analysis {
+
+    public abstract class PreAnalysis {
+
+        public ModuleDefinition TargetModule { get; private set; }
+
+        public DatabaseSchema ExternalSchema { get; private set; }
+
+        protected PreAnalysis() {
+            TargetModule = null;
+            ExternalSchema = null;
+        }
+
+        public static PreAnalysis Execute<T>(
+            ModuleDefinition module,
+            ModuleReferenceDiscovery referenceDiscovery,
+            ISchemaSerializer schemaSerializer,
+            WeaverDiagnostics diagnostics) where T : PreAnalysis, new() {
+            
+            var preAnalysis = new T();
+
+            var referenceFinder = SingleModuleReferenceFinder.Run(module, referenceDiscovery, (m) => {
+                return preAnalysis.IsTargetModule(m);
+            });
+
+            if (referenceFinder.Result == null) {
+                return preAnalysis;
+            }
+
+            preAnalysis.TargetModule = referenceFinder.Result;
+
+            var externalSchema = new DatabaseSchema();
+
+            foreach (var referenceDiscovered in referenceFinder.ModulesConsidered) {
+                var moduleSchema = preAnalysis.DiscoverSchema(referenceDiscovered, schemaSerializer);
+                if (moduleSchema != null) {
+                    externalSchema = externalSchema.MergeWith(moduleSchema);
+                }
+            }
+
+            preAnalysis.ExternalSchema = externalSchema;
+            return preAnalysis;
+        }
+
+        protected abstract bool IsTargetModule(ModuleDefinition candidate);
+
+        protected abstract DatabaseSchema DiscoverSchema(ModuleDefinition candidate, ISchemaSerializer serializer);
+    }
+}
