b8c4c5b8a9f1a21b51eab81d6ff818397373e449
Per Samuelsson <per@starcounter.com>
2017-10-11 11:45:47 +0200

    New SingleModuleReferenceFinder class and corresponding test class SingleModuleReferenceFinderTests

---
+++ src/Starcounter.Weaver/Analysis/SingleModuleReferenceFinder.cs
@@ -0,0 +1,40 @@
+﻿
+using Mono.Cecil;
+using System;
+using System.Collections.Generic;
+
+namespace Starcounter.Weaver.Analysis {
+
+    public sealed class SingleModuleReferenceFinder {
+        public ModuleDefinition Result { get; private set; }
+
+        public IEnumerable<ModuleDefinition> ModulesConsidered { get; private set; }
+
+        private SingleModuleReferenceFinder() {
+            Result = null;
+        }
+
+        public static SingleModuleReferenceFinder Run(ModuleDefinition candidate, ModuleReferenceDiscovery discovery, Func<ModuleDefinition, bool> predicate) {
+            Guard.NotNull(candidate, nameof(candidate));
+            Guard.NotNull(predicate, nameof(predicate));
+            Guard.NotNull(discovery, nameof(discovery));
+
+            var finder = new SingleModuleReferenceFinder();
+            var discovered = new List<ModuleDefinition>();
+            discovery.DiscoverReferences(candidate, (m) => {
+                if (predicate(m)) {
+                    if (finder.Result != null) {
+                        throw new Exception($"Multiple results. Cant assign {m} as {nameof(Result)}; it's already assingned to {finder.Result}");
+                    }
+
+                    finder.Result = m;
+                }
+                discovered.Add(m);
+                return true;
+            });
+
+            finder.ModulesConsidered = discovered;
+            return finder;
+        }
+    }
+}
---
+++ test/Starcounter.Weaver.Tests/ModuleReferenceDiscovery/SingleModuleReferenceFinderTests.cs
@@ -0,0 +1,40 @@
+﻿using Starcounter.Weaver.Analysis;
+using System;
+using Xunit;
+
+namespace Starcounter.Weaver.Tests {
+
+    public class SingleModuleReferenceFinderTests {
+
+        [Fact]
+        public void MultipleHitsRaiseException() {
+            var thisAssembly = TestUtilities.GetModuleOfCurrentAssembly();
+
+            int count = 0;
+            var e = Assert.Throws<Exception>(() => {
+                SingleModuleReferenceFinder.Run(
+                thisAssembly, TestUtilities.AdviceAllReferenceDiscovery, (module) => {
+                    count++;
+                    // Report each is a hit: should fail on second call.
+                    return true;
+                });
+            });
+            Assert.Equal(2, count);
+            Assert.True(e.Message.Contains("Multiple results"));
+        }
+
+
+        [Fact]
+        public void StarcounterAssemblyIsFound() {
+            var thisAssembly = TestUtilities.GetModuleOfCurrentAssembly();
+
+            var finder = SingleModuleReferenceFinder.Run(
+                thisAssembly, TestUtilities.AdviceAllReferenceDiscovery, (module) => {
+                    return module.Name.Equals("Starcounter2.dll");
+                });
+
+            Assert.NotNull(finder);
+            Assert.NotNull(finder.Result);
+        }
+    }
+}
