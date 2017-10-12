bd6bb80de9945c25c7f1a1f5ee1ab64593217036
Per Samuelsson <per@starcounter.com>
2017-10-12 15:09:12 +0200

    New type DatabaseTypeDiscoveryProvider, providing discovery of database types to clients.

---
+++ src/Starcounter.Weaver/Analysis/DatabaseTypeDiscoveryProvider.cs
@@ -0,0 +1,26 @@
+﻿
+using Mono.Cecil;
+
+namespace Starcounter.Weaver.Analysis {
+
+    public class DatabaseTypeDiscoveryProvider {
+        readonly WeaverDiagnostics diag;
+
+        public DatabaseTypeDiscoveryProvider(WeaverDiagnostics diagnostics) {
+            diag = diagnostics;
+        }
+
+        public IDatabaseTypeDiscovery ProvideDiscovery(ModuleDefinition module, PreAnalysis preAnalysis) {
+            if (preAnalysis.TargetModule == null) {
+                diag.WriteWarning($"Assembly {module} does not reference given target.");
+                return null;
+            }
+
+            return BeginDiscovery(module, preAnalysis);
+        }
+
+        protected virtual IDatabaseTypeDiscovery BeginDiscovery(ModuleDefinition module, PreAnalysis preAnalysis) {
+            return new DefaultDatabaseTypeDiscovery();
+        }
+    }
+}
