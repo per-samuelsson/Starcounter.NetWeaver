9f4ea6b3fec39176d107191ce0cf0491e2f4b23f
Per Samuelsson <per@starcounter.com>
2017-10-12 17:14:01 +0200

    New weaver ModuleAnalyzer class, orchestrating analysis.

---
+++ src/Starcounter.Weaver/ModuleAnalyzer.cs
@@ -0,0 +1,42 @@
+﻿
+using Mono.Cecil;
+using Starcounter.Hosting.Schema;
+using Starcounter.Weaver.Analysis;
+
+namespace Starcounter.Weaver {
+
+    internal class ModuleAnalyzer {
+        readonly PreAnalysis preAnalysis;
+        readonly WeaverDiagnostics diag;
+        readonly DatabaseTypeDiscoveryProvider databaseDiscoveryProvider;
+        
+        public ModuleAnalyzer(
+            PreAnalysis preAnalyser,
+            DatabaseTypeDiscoveryProvider databaseTypeDiscovery,
+            WeaverDiagnostics diagnostics) {
+
+            Guard.NotNull(preAnalyser, nameof(preAnalyser));
+            Guard.NotNull(databaseTypeDiscovery, nameof(databaseTypeDiscovery));
+            Guard.NotNull(diagnostics, nameof(diagnostics));
+
+            preAnalysis = preAnalyser;
+            diag = diagnostics;
+            databaseDiscoveryProvider = databaseTypeDiscovery;
+        }
+        
+        public DatabaseSchema DiscoverAssembly(ModuleDefinition module) {
+            ModuleDefinition targetReference;
+            DatabaseSchema schema;
+            preAnalysis.Execute(module, out targetReference, out schema);
+
+            var databaseDiscovery = databaseDiscoveryProvider.ProvideDiscovery(module, targetReference, schema);
+            if (databaseDiscovery == null) {
+                return null;
+            }
+            
+            var assembly = schema.DefineAssembly(module.Name);
+
+            return databaseDiscovery.DiscoverAssembly(module, assembly);
+        }
+    }
+}
