91c4e99357eb75f04b445ebe5eacaa1df2506bfc
Per Samuelsson <per@starcounter.com>
2017-10-12 17:12:22 +0200

    New SchemaSerialization class in Starcounter.Hosting.Schema

---
+++ src/Starcounter.Hosting/Schema/SchemaSerialization.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Starcounter.Hosting.Schema {
+    public static class SchemaSerialization {
+        public const string EmbeddedResourceStreamName = "Starcounter.Schema.V2.2017";
+
+        public static ISchemaSerializer CreateDefaultSerializer() {
+            return new JsonNETSchemaSerializer();
+        }
+    }
+}
