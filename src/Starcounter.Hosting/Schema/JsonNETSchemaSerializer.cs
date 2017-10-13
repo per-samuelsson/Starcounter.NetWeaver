00f9b116859da8c80f7036fd9958442eabbb9b7f
Per Samuelsson <per@starcounter.com>
2017-10-13 15:36:54 +0200

    Customized default serializer, filtering out what would cause circular dependencies.

--- src/Starcounter.Hosting/Schema/JsonNETSchemaSerializer.cs
+++ src/Starcounter.Hosting/Schema/JsonNETSchemaSerializer.cs
@@ -1,18 +1,48 @@
 ﻿
 using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using System.Text;
+using System.Reflection;
+using System.Collections.Generic;
 
 namespace Starcounter.Hosting.Schema {
 
     public class JsonNETSchemaSerializer : ISchemaSerializer {
+        DefaultContractResolver contractResolver;
 
+        class FilteredContractResolver : DefaultContractResolver {
+            readonly IList<string> filter;
+            
+            public FilteredContractResolver(IList<string> propertiesToIgnore) {
+                filter = propertiesToIgnore;
+            }
+
+            protected override JsonProperty CreateProperty(MemberInfo member, MemberSerialization memberSerialization) {
+                var qualifiedName = member.DeclaringType.Name + member.Name;
+                if (filter.Contains(qualifiedName)) {
+                    return null;
+                }
+                return base.CreateProperty(member, memberSerialization);
+            }
+        }
+
+        public JsonNETSchemaSerializer() {
+            contractResolver = new FilteredContractResolver(
+                new[] {
+                    typeof(DatabaseAssembly).Name + nameof(DatabaseAssembly.DefiningSchema),
+                    typeof(DatabaseType).Name + nameof(DatabaseType.DefiningAssembly),
+                    typeof(DatabaseProperty).Name + nameof(DatabaseProperty.DeclaringType)
+                    }
+                );
+        }
+        
         DatabaseSchema ISchemaSerializer.Deserialize(byte[] schema) {
             var s = Encoding.UTF8.GetString(schema);
             return JsonConvert.DeserializeObject(s) as DatabaseSchema;
         }
 
         byte[] ISchemaSerializer.Serialize(DatabaseSchema schema) {
-            var s = JsonConvert.SerializeObject(schema);
+            var s = JsonConvert.SerializeObject(schema.Assemblies, new JsonSerializerSettings() { ContractResolver = contractResolver });
             return Encoding.UTF8.GetBytes(s ?? "");
         }
     }
