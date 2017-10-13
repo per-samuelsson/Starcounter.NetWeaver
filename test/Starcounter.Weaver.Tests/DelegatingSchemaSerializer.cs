1aff21ceecc9c168edb19a2fbee1ce1c231a06a5
Per Samuelsson <per@starcounter.com>
2017-10-13 11:20:23 +0200

    New test schema serializer, DelegatingSchemaSerializer, delegating serialization/deserialization to optional callbacks.

---
+++ test/Starcounter.Weaver.Tests/DelegatingSchemaSerializer.cs
@@ -0,0 +1,24 @@
+﻿
+using Starcounter.Hosting.Schema;
+using System;
+
+namespace Starcounter.Weaver.Tests {
+
+    internal class DelegatingSchemaSerializer : ISchemaSerializer {
+        readonly Func<DatabaseSchema, byte[]> serializer;
+        readonly Func<byte[], DatabaseSchema> deserializer;
+
+        public DelegatingSchemaSerializer(Func<DatabaseSchema, byte[]> s = null, Func<byte[], DatabaseSchema> d = null) {
+            serializer = s;
+            deserializer = d;
+        }
+        
+        DatabaseSchema ISchemaSerializer.Deserialize(byte[] schema) {
+            return deserializer?.Invoke(schema);
+        }
+
+        byte[] ISchemaSerializer.Serialize(DatabaseSchema schema) {
+            return serializer?.Invoke(schema);
+        }
+    }
+}
