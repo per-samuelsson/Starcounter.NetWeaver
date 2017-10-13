59d283a47ae14360e9b82f71b8c4cdf6220b6d90
Per Samuelsson <per@starcounter.com>
2017-10-13 12:34:54 +0200

    New ModuleWriter abstraction and a AssemblyFileModuleWriter to go with that.

---
+++ src/Starcounter.Weaver/AssemblyFileModuleWriter.cs
@@ -0,0 +1,23 @@
+﻿
+using Mono.Cecil;
+
+namespace Starcounter.Weaver {
+
+    public class AssemblyFileModuleWriter : ModuleWriter {
+        protected readonly string assemblyPath;
+        protected readonly WriterParameters writeParameters;
+
+        public AssemblyFileModuleWriter(string assemblyFile, WeaverDiagnostics weaverDiagnostics, WriterParameters writerParameters = null) : base(weaverDiagnostics) {
+            Guard.FileExists(assemblyFile, nameof(assemblyFile));
+            assemblyPath = assemblyFile;
+            writeParameters = writerParameters;
+        }
+
+        // Test: path to a file in a directory that does not exist (2 levels)
+        public override void Write(ModuleDefinition module) {
+            Guard.NotNull(module, nameof(module));
+
+            module.Write(assemblyPath);
+        }
+    }
+}
---
+++ src/Starcounter.Weaver/ModuleWriter.cs
@@ -0,0 +1,16 @@
+﻿
+using Mono.Cecil;
+
+namespace Starcounter.Weaver {
+
+    public abstract class ModuleWriter {
+        protected readonly WeaverDiagnostics diagnostics;
+
+        protected ModuleWriter(WeaverDiagnostics weaverDiagnostics) {
+            Guard.NotNull(weaverDiagnostics, nameof(weaverDiagnostics));
+            diagnostics = weaverDiagnostics;
+        }
+
+        public abstract void Write(ModuleDefinition module);
+    }
+}
