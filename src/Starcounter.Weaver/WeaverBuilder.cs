48b8733d35fd63520e750b92870b55255fa56a0a
Per Samuelsson <per@starcounter.com>
2017-10-13 12:57:43 +0200

    New WeaverBuilder, simplifying composition of principal weaver.

---
+++ src/Starcounter.Weaver/WeaverBuilder.cs
@@ -0,0 +1,51 @@
+﻿using Starcounter.Hosting.Schema;
+using Starcounter.Weaver.Analysis;
+using System.IO;
+
+namespace Starcounter.Weaver {
+
+    public static class WeaverBuilder {
+        
+        /// <summary>
+        /// Creates a default weaver, weaving a given assembly file and writing
+        /// the weaved result back to a given directory.
+        /// </summary>
+        /// <param name="assemblyFile"></param>
+        /// <param name="outputDirectory"></param>
+        /// <returns></returns>
+        public static IWeaver BuildDefaultFromAssemblyFile(string assemblyFile, string outputDirectory) {
+            Guard.FileExists(assemblyFile, nameof(assemblyFile));
+            Guard.DirectoryExists(outputDirectory, nameof(outputDirectory));
+
+            var diagnostics = WeaverDiagnostics.Quiet; // TODO:
+            var readerParameters = new DefaultModuleReaderParameters(assemblyFile);
+            var moduleReader = new AssemblyFileModuleReader(assemblyFile, diagnostics, readerParameters.Parameters);
+
+            var targetPath = Path.Combine(outputDirectory, Path.GetFileName(assemblyFile));
+            var moduleWriter = new AssemblyFileModuleWriter(targetPath, diagnostics);
+
+            var schemaSerializer = new JsonNETSchemaSerializer();
+            var serializationContext = new EmbeddedResourceSchemaSerializationContext(
+                schemaSerializer,
+                EmbeddedResourceSchemaSerializationContext.DefaultResourceStreamName,
+                diagnostics
+                );
+
+            var preAnalysis = new DefaultPreAnalysis(
+                new ModuleReferenceDiscovery(ModuleReferenceDiscoveryAdvisor.PossibleDefault, diagnostics),
+                serializationContext,
+                diagnostics
+                );
+
+            var analyzer = new ModuleAnalyzer(
+                preAnalysis,
+                new DatabaseTypeDiscoveryProvider(diagnostics),
+                diagnostics
+            );
+
+            var weaver = new ModuleWeaver(serializationContext);
+
+            return new AssemblyWeaver(diagnostics, moduleReader, analyzer, weaver, moduleWriter);
+        }
+    }
+}
