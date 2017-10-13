0972cf7ea72fca4c5087bea3e7b6bb54b8885753
Per Samuelsson <per@starcounter.com>
2017-10-13 13:39:30 +0200

    New interface IDiagnosticsFormatter, allowing custom formatters for weaver diagnostics.

---
+++ src/Starcounter.Weaver/IDiagnosticsFormatter.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Starcounter.Weaver {
+
+    public interface IDiagnosticsFormatter {
+
+        string FormatError(string error, string code);
+
+        string FormatWarning(string warning, string code);
+    }
+}
