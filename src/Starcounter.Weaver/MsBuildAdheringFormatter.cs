49007c9c7f8f65e7325eff4832ea5ed1b9c83d82
Per Samuelsson <per@starcounter.com>
2017-10-13 13:42:56 +0200

    New MsBuildAdheringFormatter, writing diagnostics in well-known MsBuild error and warning format.

---
+++ src/Starcounter.Weaver/MsBuildAdheringFormatter.cs
@@ -0,0 +1,32 @@
+﻿
+namespace Starcounter.Weaver {
+
+    public class MsBuildAdheringFormatter : IDiagnosticsFormatter {
+        readonly string origin;
+        readonly string defaultWarning;
+        readonly string defaultError;
+
+        public MsBuildAdheringFormatter(string messageOrigin, string defaultWarningCode = null, string defaultErrorCode = null) {
+            Guard.NotNull(messageOrigin, nameof(messageOrigin));
+            origin = messageOrigin;
+            defaultWarning = defaultWarningCode ?? "SCW00001";
+            defaultError = defaultErrorCode ?? "SCE00001";
+        }
+
+        // Using this format:
+        // https://github.com/Microsoft/msbuild/blob/master/src/Shared/CanonicalError.cs
+        //
+        // Some background:
+        // http://blogs.msdn.com/b/msbuild/archive/2006/11/03/msbuild-visual-studio-aware-error-messages-and-message-formats.aspx
+
+        string IDiagnosticsFormatter.FormatError(string error, string code) {
+            code = string.IsNullOrWhiteSpace(code) ? defaultError : code;
+            return $"{origin}: error {code}: {error}";
+        }
+
+        string IDiagnosticsFormatter.FormatWarning(string warning, string code) {
+            code = string.IsNullOrWhiteSpace(code) ? defaultWarning : code;
+            return $"{origin}: warning {code}: {warning}";
+        }
+    }
+}
