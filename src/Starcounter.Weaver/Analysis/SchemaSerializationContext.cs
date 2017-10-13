103db88b40d70d14728800e369986241daba704f
Per Samuelsson <per@starcounter.com>
2017-10-13 11:17:38 +0200

    New SchemaSerializationContext abstraction

---
+++ src/Starcounter.Weaver/Analysis/SchemaSerializationContext.cs
@@ -0,0 +1,19 @@
+﻿
+using Mono.Cecil;
+using Starcounter.Hosting.Schema;
+
+namespace Starcounter.Weaver.Analysis {
+
+    public abstract class SchemaSerializationContext {
+        protected readonly WeaverDiagnostics diagnostics;
+
+        protected SchemaSerializationContext(WeaverDiagnostics weaverDiagnostics) {
+            Guard.NotNull(weaverDiagnostics, nameof(weaverDiagnostics));
+            diagnostics = weaverDiagnostics;
+        }
+
+        public abstract DatabaseSchema Read(ModuleDefinition module);
+
+        public abstract void Write(ModuleDefinition module, DatabaseSchema schema);
+    }
+}
