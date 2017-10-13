378fee08ca3a2a6b0e67be43766e8ba76d1923dc
Per Samuelsson <per@starcounter.com>
2017-10-13 12:20:13 +0200

    New ModuleReader and AssemblyFileModuleReader classes.

---
+++ src/Starcounter.Weaver/AssemblyFileModuleReader.cs
@@ -0,0 +1,20 @@
+﻿
+using Mono.Cecil;
+
+namespace Starcounter.Weaver {
+
+    public class AssemblyFileModuleReader : ModuleReader {
+        protected readonly string assemblyPath;
+        protected readonly ReaderParameters readParameters;
+
+        public AssemblyFileModuleReader(string assemblyFile, WeaverDiagnostics weaverDiagnostics, ReaderParameters readerParameters = null) : base(weaverDiagnostics) {
+            Guard.FileExists(assemblyFile, nameof(assemblyFile));
+            assemblyPath = assemblyFile;
+            readParameters = readerParameters;
+        }
+
+        public override ModuleDefinition Read() {
+            return ModuleDefinition.ReadModule(assemblyPath, readParameters);
+        }
+    }
+}
---
+++ src/Starcounter.Weaver/ModuleReader.cs
@@ -0,0 +1,15 @@
+﻿using Mono.Cecil;
+
+namespace Starcounter.Weaver {
+
+    public abstract class ModuleReader {
+        protected readonly WeaverDiagnostics diagnostics;
+
+        protected ModuleReader(WeaverDiagnostics weaverDiagnostics) {
+            Guard.NotNull(weaverDiagnostics, nameof(weaverDiagnostics));
+            diagnostics = weaverDiagnostics;
+        }
+
+        public abstract ModuleDefinition Read();
+    }
+}
