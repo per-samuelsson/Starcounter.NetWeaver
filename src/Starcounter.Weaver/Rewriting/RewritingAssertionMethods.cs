376ad6c9d273a3346b54c0cb5ae4b78409a3d88b
Per Samuelsson <per@starcounter.com>
2017-11-06 10:43:49 +0100

    Some assertion methods we should use to assert/diagnose rewriting presumptions.

---
+++ src/Starcounter.Weaver/Rewriting/RewritingAssertionMethods.cs
@@ -0,0 +1,64 @@
+﻿
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Starcounter.Weaver.Rewriting {
+
+    public static class WeaverAssertionMethods {
+
+        public static void VerifyExpectedOriginalGetter(MethodDefinition getter) {
+            Guard.NotNull(getter, nameof(getter));
+            VerifyBodyHasExpectedSequence(getter, new[] {
+                OpCodes.Ldarg_0,
+                OpCodes.Ldfld,
+                OpCodes.Ret
+            });
+        }
+
+        public static void VerifyExpectedOriginalSetter(MethodDefinition setter) {
+            Guard.NotNull(setter, nameof(setter));
+            VerifyBodyHasExpectedSequence(setter, new[] {
+                OpCodes.Ldarg_0,
+                OpCodes.Ldarg_1,
+                OpCodes.Stfld,
+                OpCodes.Ret
+            });
+        }
+
+        public static bool InstructionsHasExactSequence(IEnumerable<Instruction> instructions, IEnumerable<OpCode> codes) {
+            Guard.NotNull(instructions, nameof(instructions));
+            Guard.NotNull(codes, nameof(codes));
+
+            var first = instructions.ToArray();
+            var second = codes.ToArray();
+
+            if (first.Length != second.Length) {
+                return false;
+            }
+
+            for (int i = 0; i < first.Length; i++) {
+                if (first[i].OpCode != second[i]) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        static void VerifyBodyHasExpectedSequence(MethodDefinition method, OpCode[] sequence) {
+            if (!method.HasBody) {
+                throw new ArgumentException($"Method {method.Name} has no body", nameof(method));
+            }
+
+            var instructions = method.Body.Instructions.Where(i => i.OpCode != OpCodes.Nop);
+
+            var match = InstructionsHasExactSequence(instructions, sequence);
+            if (!match) {
+                throw new ArgumentException($"Body of method {method.Name} does not match expected sequence");
+            }
+        }
+    }
+}
