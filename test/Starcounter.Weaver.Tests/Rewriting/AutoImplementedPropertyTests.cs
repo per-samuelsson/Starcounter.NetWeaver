cf4f87c2f6ae0b61c9b3f73c2994228491b939d0
Per Samuelsson <per@starcounter.com>
2017-11-07 09:03:27 +0100

    New AutoImplementedPropertyTests class.

---
+++ test/Starcounter.Weaver.Tests/Rewriting/AutoImplementedPropertyTests.cs
@@ -0,0 +1,36 @@
+﻿
+using Starcounter.Weaver.Rewriting;
+using System.Linq;
+using Xunit;
+
+namespace Starcounter.Weaver.Tests {
+
+    public class AutoImplementedPropertyTests {
+
+        [Fact]
+        public void AllPropertiesOfIntAutoPropertiesIsAutoImplemented() {
+            var module = TestUtilities.GetModuleOfCurrentAssembly();
+
+            var type = module.Types.Single(t => t.FullName == typeof(IntAutoProperties).FullName);
+            Assert.NotNull(type);
+
+            Assert.True(type.Properties.All(p => p.IsAutoImplemented()));
+        }
+
+        [Fact]
+        public void AllPropertiesOfIntAutoPropertiesHasExpectedImplementation() {
+            var module = TestUtilities.GetModuleOfCurrentAssembly();
+
+            var type = module.Types.Single(t => t.FullName == typeof(IntAutoProperties).FullName);
+            Assert.NotNull(type);
+
+            Assert.True(type.Properties.All(p => {
+                RewritingAssertionMethods.VerifyExpectedOriginalGetter(p.GetMethod);
+                if (p.SetMethod != null) {
+                    RewritingAssertionMethods.VerifyExpectedOriginalSetter(p.SetMethod);
+                }
+                return true;
+            }));
+        }
+    }
+}
