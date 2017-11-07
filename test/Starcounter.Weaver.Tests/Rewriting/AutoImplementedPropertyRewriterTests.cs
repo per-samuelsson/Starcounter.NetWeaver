ea7a69e731f984987e928fe99329c43a6c6ca168
Per Samuelsson <per@starcounter.com>
2017-11-07 15:43:00 +0100

    New set of tests: AutoImplementedPropertyRewriterTests.

---
+++ test/Starcounter.Weaver.Tests/Rewriting/AutoImplementedPropertyRewriterTests.cs
@@ -0,0 +1,56 @@
+﻿
+using Starcounter.Weaver.Rewriting;
+using System.Linq;
+using Xunit;
+
+namespace Starcounter.Weaver.Tests {
+
+    public class ClassWithIntAutoProperty {
+
+        public int Int1 { get; set; }
+
+        public static int GetInt(ulong i1, ulong i2, ulong i3) {
+            return 42;
+        }
+
+        public static void SetInt(ulong i1, ulong i2, ulong i3, int v) {
+        }
+    }
+
+    public class AutoImplementedPropertyRewriterTests {
+        
+        [Fact]
+        public void RewriterAcceptStateWithReferenceFields() {
+            var module = TestUtilities.GetModuleOfCurrentAssembly();
+
+            var type = module.Types.Single(t => t.FullName == typeof(ClassWithIntAutoProperty).FullName);
+            Assert.NotNull(type);
+            
+            var state = new DatabaseTypeStateEmitter(type, new DatabaseTypeStateNames());
+            state.EmitReferenceFields();
+            
+            new AutoImplementedPropertyRewriter(state);
+        }
+
+        [Fact]
+        public void Test2() {
+            var module = TestUtilities.GetModuleOfCurrentAssembly();
+
+            var type = module.Types.Single(t => t.FullName == typeof(ClassWithIntAutoProperty).FullName);
+            Assert.NotNull(type);
+
+            var property = type.Properties.Single(p => p.Name == nameof(ClassWithIntAutoProperty.Int1));
+            var readMethod = type.Methods.Single(m => m.Name == nameof(ClassWithIntAutoProperty.GetInt));
+            var writeMethod = type.Methods.Single(m => m.Name == nameof(ClassWithIntAutoProperty.SetInt));
+
+            var state = new DatabaseTypeStateEmitter(type, new DatabaseTypeStateNames());
+            state.EmitReferenceFields();
+            state.EmitPropertyCRUDHandle(nameof(ClassWithIntAutoProperty.Int1));
+
+            var autoProperty = new AutoImplementedProperty(property);
+            var rewriter = new AutoImplementedPropertyRewriter(state);
+            
+            rewriter.Rewrite(autoProperty, readMethod, writeMethod);
+        }
+    }
+}
