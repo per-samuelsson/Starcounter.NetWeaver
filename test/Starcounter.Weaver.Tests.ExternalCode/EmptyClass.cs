807d07ffd8137a186861448295d976a30ff89543
Per Samuelsson <per@starcounter.com>
2017-11-07 12:28:18 +0100

    New test class ClassWithExplicitStateReferenceFields

--- test/Starcounter.Weaver.Tests.ExternalCode/EmptyClass.cs
+++ test/Starcounter.Weaver.Tests.ExternalCode/EmptyClass.cs
@@ -3,4 +3,9 @@ namespace Starcounter.Weaver.Tests.ExternalCode {
     
     public class EmptyClass {
     }
+
+    public class ClassWithExplicitStateReferenceFields {
+        protected ulong dbId = 42;
+        protected ulong dbRef = 42;
+    }
 }
