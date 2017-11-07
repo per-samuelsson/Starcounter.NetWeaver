c2a7796fcd61c77d6b8585df877ce5d305365e87
Per Samuelsson <per@starcounter.com>
2017-11-07 12:28:02 +0100

    Yet another test library, adding one extra layer of inheritance, for us to be able to test cases where weaving assembly with indirect references to already weaved base-base class.

---
+++ test/Starcounter.Weaver.Tests.ExternalCode/EmptyClassDerivingBase.cs
@@ -0,0 +1,11 @@
+﻿using Starcounter.Weaver.Tests.ExternalCodeBase;
+
+namespace Starcounter.Weaver.Tests.ExternalCode {
+
+    public class EmptyClassDerivingBase : EmptyClassBase {
+    }
+
+    public class ClassDerivingExplicitStateReferenceFieldsBase : ClassWithExplicitStateReferenceFieldsBase {
+
+    }
+}
---
+++ test/Starcounter.Weaver.Tests.ExternalCodeBase/EmptyClassBase.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Starcounter.Weaver.Tests.ExternalCodeBase {
+    
+    public class EmptyClassBase {
+    }
+
+    public class ClassWithExplicitStateReferenceFieldsBase {
+        protected ulong dbId = 42;
+        protected ulong dbRef = 42;
+    }
+}
