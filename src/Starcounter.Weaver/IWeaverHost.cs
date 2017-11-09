7b046bfd1514d827fd149508fad8df8ffc0d67f6
Per Samuelsson <per@starcounter.com>
2017-11-09 13:54:00 +0100

    New IWeaverHost abstraction

---
+++ src/Starcounter.Weaver/IWeaverHost.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Starcounter.Weaver {
+
+    /// <summary>
+    /// Represent the running weaver. Provided to weaver implementations
+    /// during weaving.
+    /// </summary>
+    public interface IWeaverHost {
+
+        WeaverDiagnostics Diagnostics { get; }
+    }
+}
