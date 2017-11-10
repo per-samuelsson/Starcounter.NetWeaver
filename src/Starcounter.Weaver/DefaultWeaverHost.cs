8048b52eb0290a4686c34e1c3d9577a4b7d3e147
Per Samuelsson <per@starcounter.com>
2017-11-10 08:33:05 +0100

    New DefaultWeaverHost

---
+++ src/Starcounter.Weaver/DefaultWeaverHost.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Starcounter.Weaver {
+
+    public class DefaultWeaverHost : IWeaverHost {
+        readonly WeaverDiagnostics diagnostics;
+
+        public WeaverDiagnostics Diagnostics => diagnostics;
+
+        public DefaultWeaverHost(WeaverDiagnostics weaverDiagnostics) {
+            Guard.NotNull(weaverDiagnostics, nameof(weaverDiagnostics));
+            diagnostics = weaverDiagnostics;
+        }
+    }
+}
