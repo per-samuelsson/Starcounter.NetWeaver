c8e4698829c1399356f7c20b88b0c83782a53a54
Per Samuelsson <per@starcounter.com>
2017-11-13 17:36:02 +0100

    DatabaseSchema now define notion of named data types, and allow them to be registered.

--- src/Starcounter.Hosting/Schema/DatabaseSchema.cs
+++ src/Starcounter.Hosting/Schema/DatabaseSchema.cs
@@ -1,15 +1,51 @@
 ﻿
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Starcounter.Hosting.Schema {
 
     public sealed class DatabaseSchema {
         Dictionary<string, DatabaseAssembly> assemblies = new Dictionary<string, DatabaseAssembly>();
+        List<string> dataTypes = new List<string>();
+
+        class NamedType : IDataType {
+            readonly string name;
+
+            public string Name => name;
+
+            public NamedType(string typeName) {
+                name = typeName;
+            }
+        }
         
         public IEnumerable<DatabaseAssembly> Assemblies {
             get {
                 return assemblies.Values;
+            }
+        }
+
+        public IEnumerable<IDataType> DataTypes {
+            get {
+                var databaseTypeCount = assemblies.Values.Sum(a => a.Types.Count());
+                var types = new List<IDataType>(dataTypes.Count + databaseTypeCount);
+                foreach (var name in dataTypes) {
+                    types.Add(new NamedType(name));
+                }
+                foreach (var assembly in assemblies.Values) {
+                    types.AddRange(assembly.Types);
+                }
+                return types;
+            }
+        }
+
+        public void DefineDataType(string name) {
+            if (string.IsNullOrWhiteSpace(name)) {
+                throw new ArgumentNullException(nameof(name));
+            }
+
+            if (!dataTypes.Contains(name)) {
+                dataTypes.Add(name);
             }
         }
         
@@ -44,7 +80,7 @@ namespace Starcounter.Hosting.Schema {
             return true;
         }
 
-        internal DatabaseType FindType(string name) {
+        public DatabaseType FindType(string name) {
             if (string.IsNullOrWhiteSpace(name)) {
                 throw new ArgumentNullException(nameof(name));
             }
@@ -61,6 +97,11 @@ namespace Starcounter.Hosting.Schema {
             }
 
             return null;
+        }
+
+        internal bool TryGetIndexOfDataType(string name, out int index) {
+            index = dataTypes.IndexOf(name);
+            return index != -1;
         }
     }
 }
