bb1d80bc34c434fe42b9c2bfecc5e58bcdf9f589
Per Samuelsson <per@starcounter.com>
2017-11-16 09:49:39 +0100

    New test TypeDefintitionBindingExtensionsTests.TypesDerivingObjectShouldBindWithExpectedBaseName

---
+++ test/starweave.Tests/TypeDefintitionBindingExtensionsTests.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Linq;
+using Xunit;
+
+namespace starweave.Weaver.Tests {
+
+    public class ClassWithNoDefinedBaseClass {
+
+    }
+
+    public class TypeDefintitionBindingExtensionsTests {
+
+        [Fact]
+        public void TypesDerivingObjectShouldBindWithExpectedBaseName() {
+            var module = TestUtilities.GetModuleOfCurrentAssembly();
+
+            var type = module.Types.Single(t => t.FullName == typeof(ClassWithNoDefinedBaseClass).FullName);
+            Assert.NotNull(type);
+
+            Assert.Equal(TypeDefintitionBindingExtensions.BindingNameOfNoDeclaredBaseType, type.GetBaseTypeBindingName());
+        }
+    }
+}
