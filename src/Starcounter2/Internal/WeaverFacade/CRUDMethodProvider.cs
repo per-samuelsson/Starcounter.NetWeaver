169c978ab7c49ef243046e1933696390dee5cc9f
Per Samuelsson <per@starcounter.com>
2017-11-17 16:38:46 +0100

    New CRUDMethodProvider abstraction and first default implementation of it for Starcounter2.

---
+++ src/Starcounter2/Internal/WeaverFacade/CRUDMethodProvider.cs
@@ -0,0 +1,44 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Starcounter2.Internal.WeaverFacade {
+
+    public abstract class CRUDMethodProvider {
+
+        public abstract string CreateMethod { get; }
+
+        public abstract Dictionary<string, string> ReadMethods { get; }
+
+        public abstract Dictionary<string, string> UpdateMethods { get; }
+
+        public abstract string DeleteMethod { get; }
+
+        public IEnumerable<string> SupportedDataTypes {
+            get {
+                return ReadMethods.Keys.Union(UpdateMethods.Keys);
+            }
+        }
+
+        public abstract MethodInfo GetMethodByName(string method);
+
+        public virtual MethodInfo GetCreateMethod() {
+            return GetMethodByName(CreateMethod);
+        }
+
+        public virtual MethodInfo GetReadMethod(string dataType) {
+            var methodName = ReadMethods[dataType];
+            return GetMethodByName(methodName);
+        }
+
+        public virtual MethodInfo GetUpdateMethod(string dataType) {
+            var methodName = UpdateMethods[dataType];
+            return GetMethodByName(methodName);
+        }
+
+        public virtual MethodInfo GetDeleteMethod(string dataType) {
+            return GetMethodByName(DeleteMethod);
+        }
+    }
+}
---
+++ src/Starcounter2/Internal/WeaverFacade/DefaultCRUDMethodProvider.cs
@@ -0,0 +1,47 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Starcounter2.Internal.WeaverFacade {
+
+    public class DefaultCRUDMethodProvider : CRUDMethodProvider {
+        readonly static Dictionary<string, string> readMethods = new Dictionary<string, string>() {
+            { typeof(int).FullName, nameof(CRUD.GetInt) },
+            { typeof(int?).FullName, nameof(CRUD.GetNullableInt) }
+        };
+
+        readonly static Dictionary<string, string> writeMethods = new Dictionary<string, string>() {
+            { typeof(int).FullName, nameof(CRUD.SetInt) },
+            { typeof(int?).FullName, nameof(CRUD.SetNullableInt) }
+        };
+        
+        public override string CreateMethod {
+            get {
+                return nameof(CRUD.CreateCrud);
+            }
+        }
+
+        public override Dictionary<string, string> ReadMethods {
+            get {
+                return readMethods;
+            }
+        }
+
+        public override Dictionary<string, string> UpdateMethods {
+            get {
+                return writeMethods;
+            }
+        }
+
+        public override string DeleteMethod {
+            get {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override MethodInfo GetMethodByName(string method) {
+            return typeof(CRUD).GetTypeInfo().GetMethod(method, BindingFlags.Public | BindingFlags.Static);
+        }
+    }
+}
