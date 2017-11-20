1f2b3d74645d61855b75429fd03ba3221ec86dd3
Per Samuelsson <per@starcounter.com>
2017-11-20 09:18:42 +0100

    New pair of test projects ConsoleAppNetCoreSingleDbProperty and ConsoleAppNetFrameworkSingleDbProperty.

---
+++ test/TestProjects/ConsoleAppNetCoreSingleDbProperty/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using Starcounter2;
+
+namespace ConsoleAppNetCoreSingleDbProperty {
+
+    [Database]
+    public class Foo {
+        public int Bar { get; set; }
+    }
+
+    class Program {
+        static void Main(string[] args) {
+            var f = new Foo();
+            f.Bar = 42;
+            var v = f.Bar;
+        }
+    }
+}
