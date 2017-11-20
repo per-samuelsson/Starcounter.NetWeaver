f37c6a30a2c4aeb3c52e053983cb8d270ef79f40
Per Samuelsson <per@starcounter.com>
2017-11-20 13:47:43 +0100

    New CodeEmissionContext class, abstraction over Cecil reference imports.

---
+++ src/Starcounter.Weaver/CodeEmissionContext.cs
@@ -0,0 +1,28 @@
+﻿
+using Mono.Cecil;
+using System;
+
+namespace Starcounter.Weaver {
+
+    /// <summary>
+    /// Provide any rewriting code an abstraction over Cecil
+    /// reference importing until we have figured out how to best
+    /// deal with it, including how duplicate imports behave and
+    /// cost.
+    /// </summary>
+    public sealed class CodeEmissionContext {
+        readonly ModuleDefinition module;
+
+        public CodeEmissionContext(ModuleDefinition emissionTargetModule) {
+            module = emissionTargetModule ?? throw new ArgumentNullException(nameof(emissionTargetModule));
+        }
+
+        public TypeReference Use(TypeReference type) {
+            return module.ImportReference(type);
+        }
+
+        public MethodReference Use(MethodReference method) {
+            return module.ImportReference(method);
+        }
+    }
+}
