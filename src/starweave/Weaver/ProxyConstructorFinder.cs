e6f6ca9eb44dcc7dcb68e5a29001140d26c10e90
Per Samuelsson <per@starcounter.com>
2017-12-13 14:48:00 +0100

    New ProxyConstructorFinder type, used when emitted proxy constructors need to be found.

---
+++ src/starweave/Weaver/ProxyConstructorFinder.cs
@@ -0,0 +1,26 @@
+﻿
+using Mono.Cecil;
+using Starcounter.Weaver;
+using System;
+using System.Linq;
+
+namespace starweave.Weaver {
+
+    public class ProxyConstructorFinder {
+        protected readonly TypeReference signatureType;
+
+        public ProxyConstructorFinder(TypeReference signatureTypeReference) {
+            signatureType = signatureTypeReference ?? throw new ArgumentNullException(nameof(signatureTypeReference));
+        }
+
+        public MethodReference FindProxyConstructor(TypeDefinition type) {
+            return type.GetInstanceConstructors().FirstOrDefault(c => {
+                return HasProxyConstructorSignature(c, signatureType);
+            });
+        }
+
+        public static bool HasProxyConstructorSignature(MethodDefinition ctor, TypeReference signatureType) {
+            return ctor.HasParameters && ctor.Parameters.Count == 1 && ctor.Parameters[0].ParameterType.ReferenceSameType(signatureType);
+        }
+    }
+}
