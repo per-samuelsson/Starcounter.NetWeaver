8aaf8a3f857abe8b60381e1c082fce6488790b36
Per Samuelsson <per@starcounter.com>
2017-12-14 17:30:13 +0100

    New type ConstructorSignatureTypes.

---
+++ src/starweave/Weaver/ConstructorSignatureTypes.cs
@@ -0,0 +1,26 @@
+﻿
+using Mono.Cecil;
+using Starcounter.Weaver;
+using System;
+using System.Collections.Generic;
+
+namespace starweave.Weaver {
+
+    public class ConstructorSignatureTypes {
+
+        public TypeReference ProxyConstructorParameter { get; }
+
+        public TypeReference InsertConstructorParameter { get; }
+
+        public IEnumerable<TypeReference> Types {
+            get {
+                return new[] { ProxyConstructorParameter, InsertConstructorParameter };
+            }
+        }
+
+        public ConstructorSignatureTypes(CodeEmissionContext emitContext, Type proxyConstructorParameterType, Type insertConstructorParameterType) {
+            ProxyConstructorParameter = emitContext.Use(proxyConstructorParameterType);
+            InsertConstructorParameter = emitContext.Use(insertConstructorParameterType);
+        }
+    }
+}
