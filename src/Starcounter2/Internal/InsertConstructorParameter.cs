e8fb1807cf534b2dff8398fc9e0d1628965dad4a
Per Samuelsson <per@starcounter.com>
2017-12-14 09:28:10 +0100

    New InsertConstructorParameter type, used to assure unique signature of the emitted insert constructor.

---
+++ src/Starcounter2/Internal/InsertConstructorParameter.cs
@@ -0,0 +1,8 @@
+﻿
+namespace Starcounter2.Internal {
+
+    /// <summary>
+    /// Type used to assure unique signature of the emitted insert constructor.
+    /// </summary>
+    public sealed class InsertConstructorParameter {}
+}
