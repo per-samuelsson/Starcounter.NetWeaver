3cfde7b99c616fcfcd518ec838119c3cce6b41e9
Per Samuelsson <per@starcounter.com>
2017-12-14 09:23:03 +0100

    New type ProxyConstructorParameter, used to assure unique signature of all proxy constructors.

---
+++ src/Starcounter2/Internal/ProxyConstructorParameter.cs
@@ -0,0 +1,8 @@
+﻿
+namespace Starcounter2.Internal {
+
+    /// <summary>
+    /// Type used to assure unique signature of emitted proxy constructors.
+    /// </summary>
+    public sealed class ProxyConstructorParameter { }
+}
