8b6f820f19b2d1682ba0a29b8f9d850c95a5a113
Per Samuelsson <per@starcounter.com>
2017-12-15 14:51:57 +0100

    Extended ReplacementConstructorEmitterTests with more testing.

--- test/starweave.Tests/ReplacementConstructorEmitterTests.cs
+++ test/starweave.Tests/ReplacementConstructorEmitterTests.cs
@@ -50,10 +50,10 @@ namespace starweave.Tests {
                 var uniqueType = module.Types.Single(t => t.FullName == typeof(ReplaceUniqueType).FullName);
                 var targetType = module.Types.Single(t => t.FullName == typeof(ReplaceCtorTestNoneDefined).FullName);
                 var original = targetType.Methods.Single(m => m.Name == ".ctor");
-
+                
                 var emitter = new ReplacementConstructorEmitter(new CodeEmissionContext(module), uniqueType);
                 var replacement = emitter.Emit(original);
-                AssertExpectedReplacement(original, replacement);
+                AssertExpectedReplacementBeforeRedirect(original, replacement);
             }
         }
 
@@ -65,22 +65,72 @@ namespace starweave.Tests {
                 var uniqueType = module.Types.Single(t => t.FullName == typeof(ReplaceUniqueType).FullName);
                 var targetType = module.Types.Single(t => t.FullName == typeof(ReplaceCtorTestCustomConstructors).FullName);
                 var originals = targetType.Methods.Where(m => m.Name == ".ctor");
+                
+                var emitter = new ReplacementConstructorEmitter(new CodeEmissionContext(module), uniqueType);
+                var arr = originals.ToArray();
+
+                foreach (var original in arr) {
+                    var replacement = emitter.Emit(original);
+                    AssertExpectedReplacementBeforeRedirect(original, replacement);
+                }
+            }
+        }
+
+        [Fact]
+        public void ReplaceReimplementOriginalDefaultConstructor() {
+            using (var moduleHandle = TestUtilities.GetModuleOfCurrentAssemblyForRewriting()) {
+                var module = moduleHandle.Module;
+
+                var uniqueType = module.Types.Single(t => t.FullName == typeof(ReplaceUniqueType).FullName);
+                var targetType = module.Types.Single(t => t.FullName == typeof(ReplaceCtorTestNoneDefined).FullName);
+                var original = targetType.Methods.Single(m => m.Name == ".ctor");
+
+                var state = new DatabaseTypeStateEmitter(new CodeEmissionContext(module), targetType, new DatabaseTypeStateNames());
+                state.EmitCRUDHandles();
+                state.EmitReferenceFields();
+
+                var emitter = new ReplacementConstructorEmitter(new CodeEmissionContext(module), uniqueType);
+                var replacement = emitter.Emit(original);
+                AssertExpectedReplacementBeforeRedirect(original, replacement);
+                emitter.Redirect(original, replacement, state);
+                var call = MethodCallFinder.FindSingleCallToAnyTarget(original, new[] { replacement });
+                Assert.NotNull(call);
+            }
+        }
+
+        [Fact]
+        public void ReplaceReimplementOriginalForCustomConstructors() {
+            using (var moduleHandle = TestUtilities.GetModuleOfCurrentAssemblyForRewriting()) {
+                var module = moduleHandle.Module;
+
+                var uniqueType = module.Types.Single(t => t.FullName == typeof(ReplaceUniqueType).FullName);
+                var targetType = module.Types.Single(t => t.FullName == typeof(ReplaceCtorTestCustomConstructors).FullName);
+                var originals = targetType.Methods.Where(m => m.Name == ".ctor");
+
+                var state = new DatabaseTypeStateEmitter(new CodeEmissionContext(module), targetType, new DatabaseTypeStateNames());
+                state.EmitCRUDHandles();
+                state.EmitReferenceFields();
 
                 var emitter = new ReplacementConstructorEmitter(new CodeEmissionContext(module), uniqueType);
                 var arr = originals.ToArray();
 
                 foreach (var original in arr) {
                     var replacement = emitter.Emit(original);
-                    AssertExpectedReplacement(original, replacement);
+                    AssertExpectedReplacementBeforeRedirect(original, replacement);
+                    emitter.Redirect(original, replacement, state);
+                    var call = MethodCallFinder.FindSingleCallToAnyTarget(original, new[] { replacement });
+                    Assert.NotNull(call);
                 }
             }
         }
 
-        void AssertExpectedReplacement(MethodDefinition original, MethodDefinition replacement) {
+        // Assert replacement constructor matches original before original has
+        // been redirected.
+        void AssertExpectedReplacementBeforeRedirect(MethodDefinition original, MethodDefinition replacement) {
             Assert.NotNull(replacement);
             Assert.True(replacement.DeclaringType.FullName.Equals(original.DeclaringType.FullName));
-            Assert.Equal(replacement.Body.Instructions.Count, original.Body.Instructions.Count);
             Assert.Equal(replacement.Parameters.Count, original.Parameters.Count + 2);
+            Assert.Equal(replacement.Body.Instructions.Count, original.Body.Instructions.Count);
         }
     }
 }
