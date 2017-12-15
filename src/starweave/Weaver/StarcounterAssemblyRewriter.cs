58646adecc1debcae07c9959955f6f7cdec7f2da
Per Samuelsson <per@starcounter.com>
2017-12-15 10:01:23 +0100

    Incorporated constructor rewriter in StarcounterAssemblyRewriter

--- src/starweave/Weaver/StarcounterAssemblyRewriter.cs
+++ src/starweave/Weaver/StarcounterAssemblyRewriter.cs
@@ -12,6 +12,7 @@ namespace starweave.Weaver {
         readonly DatabaseTypeStateNames names;
         readonly DbCrudMethodProvider crudMethods;
         readonly CodeEmissionContext emitContext;
+        readonly DatabaseTypeConstructorRewriter constructorRewriter;
 
         public StarcounterAssemblyRewriter(IWeaverHost weaverHost, AnalysisResult result, DatabaseTypeStateNames stateNames, DbCrudMethodProvider methodProvider) {
             host = weaverHost ?? throw new ArgumentNullException(nameof(weaverHost));
@@ -19,17 +20,33 @@ namespace starweave.Weaver {
             names = stateNames ?? throw new ArgumentNullException(nameof(stateNames));
             crudMethods = methodProvider ?? throw new ArgumentNullException(nameof(methodProvider));
             emitContext = new CodeEmissionContext(result.SourceModule);
+
+            // We might eventually want to inject these. They will differ between L1 and Nova
+            // and are part of API we want to make sure it's versioned.
+            constructorRewriter = new DatabaseTypeConstructorRewriter(
+                host,
+                emitContext,
+                typeof(Starcounter2.Internal.ProxyConstructorParameter),
+                typeof(Starcounter2.Internal.InsertConstructorParameter),
+                crudMethods.GetCreateMethod()
+            );
         }
         
         void IAssemblyRewriter.RewriteType(DatabaseType type) {
             var module = analysis.SourceModule;
 
             var typeDef = type.GetTypeDefinition(module);
+            var baseType = type.GetBaseType();
+            var baseTypeDef = baseType != null ? typeDef.BaseType.Resolve() : null;
 
             var stateEmitter = new DatabaseTypeStateEmitter(emitContext, typeDef, names);
             stateEmitter.EmitCRUDHandles();
-            stateEmitter.EmitReferenceFields();
+            if (baseType == null) {
+                stateEmitter.EmitReferenceFields();
+            }
 
+            constructorRewriter.Rewrite(typeDef, baseTypeDef, stateEmitter);
+            
             var propRewriter = new AutoImplementedPropertyRewriter(stateEmitter);
 
             foreach (var databaseProperty in type.Properties) {
@@ -43,6 +60,10 @@ namespace starweave.Weaver {
                 // get method of "reference" / "object". The named data type must
                 // support that interface. Also: there will need to be a cast in
                 // the getter.
+                // TODO:
+
+                // Move to rewrite? We can't know for sure they will be used,
+                // and hence closer is better.
                 // TODO:
 
                 var readMethod = crudMethods.GetReadMethod(databaseProperty.DataType.Name);
