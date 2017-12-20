b32e89e3bdef6392492ac96e125005405fee8c1d
Per Samuelsson <per@starcounter.com>
2017-12-20 08:32:36 +0100

    New starweave abstraction, TargetRuntimeFacadeProvider, provisioning weaver with IAssemblyRuntimeFacade implementations.

---
+++ src/starweave/Weaver/TargetRuntimeFacadeProvider.cs
@@ -0,0 +1,16 @@
+﻿
+using Mono.Cecil;
+using Starcounter.Hosting;
+
+namespace starweave.Weaver {
+
+    /// <summary>
+    /// Provider of IAssemblyRuntimeFacade implementations.
+    /// </summary>
+    public abstract class TargetRuntimeFacadeProvider {
+
+        public abstract bool IsTargetRuntimeReference(ModuleDefinition module);
+
+        public abstract IAssemblyRuntimeFacade ProvideRuntimeFacade(ModuleDefinition targetReference);
+    }
+}
