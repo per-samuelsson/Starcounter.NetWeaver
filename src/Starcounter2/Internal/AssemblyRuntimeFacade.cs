e7e3df084270cbd432bddb4dc7d7b52b65f6f978
Per Samuelsson <per@starcounter.com>
2017-12-20 09:53:33 +0100

    New IAssemblyRuntimeFacade implementation in Starcounter2.

---
+++ src/Starcounter2/Internal/AssemblyRuntimeFacade.cs
@@ -0,0 +1,30 @@
+﻿
+using Starcounter.Hosting;
+using System;
+using System.Reflection;
+
+namespace Starcounter2.Internal {
+    public sealed class AssemblyRuntimeFacade : IAssemblyRuntimeFacade {
+        readonly DbCrudMethodProvider crudProvider;
+
+        public AssemblyRuntimeFacade() {
+            crudProvider = new DefaultDbCrudMethodProvider();
+        }
+
+        public Type DatabaseAttributeType => typeof(DatabaseAttribute);
+
+        public Type ProxyConstructorSignatureType => typeof(ProxyConstructorParameter);
+
+        public Type InsertConstructorSignatureType => typeof(InsertConstructorParameter);
+
+        public MethodInfo CreateMethod => crudProvider.GetCreateMethod();
+
+        public MethodInfo GetReadMethod(string type) {
+            return crudProvider.GetReadMethod(type);
+        }
+
+        public MethodInfo GetWriteMethod(string type) {
+            return crudProvider.GetUpdateMethod(type);
+        }
+    }
+}
