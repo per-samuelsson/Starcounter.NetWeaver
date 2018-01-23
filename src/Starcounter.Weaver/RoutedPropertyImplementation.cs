053d882862c8bd5db88a1a51a437f68439665f7f
Per Samuelsson <per@starcounter.com>
2018-01-23 10:16:39 +0100

    Added support for routing of interface properties via new type RoutedPropertyImplementation.

---
+++ src/Starcounter.Weaver/RoutedPropertyImplementation.cs
@@ -0,0 +1,38 @@
+﻿
+using Mono.Cecil;
+
+namespace Starcounter.Weaver {
+
+    public class RoutedPropertyImplementation {
+        readonly InterfaceImplementation interfaceImplementation;
+        readonly PropertyDefinition interfaceProperty;
+
+        public PropertyDefinition ImplementedProperty {
+            get;
+            private set;
+        }
+
+        public RoutedPropertyImplementation(InterfaceImplementation implementation, PropertyDefinition property) {
+            Guard.NotNull(implementation, nameof(implementation));
+            Guard.NotNull(property, nameof(property));
+            interfaceProperty = property;
+            interfaceImplementation = implementation;
+        }
+
+        public void ImplementOn(
+            TypeDefinition type,
+            RoutedMethodImplementation getter,
+            RoutedMethodImplementation setter = null) {
+            Guard.NotNull(type, nameof(type));
+            Guard.NotNull(getter, nameof(getter));
+
+            var p = new PropertyDefinition(interfaceProperty.Name, interfaceProperty.Attributes, interfaceProperty.PropertyType) {
+                GetMethod = getter.ImplementedMethod,
+                SetMethod = setter?.ImplementedMethod
+            };
+            type.Properties.Add(p);
+
+            ImplementedProperty = p;
+        }
+    }
+}
