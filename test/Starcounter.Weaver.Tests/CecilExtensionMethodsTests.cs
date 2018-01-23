2be5e347fb06983238b8f0dba6bbf25b226225ec
Per Samuelsson <per@starcounter.com>
2018-01-23 12:29:21 +0100

    New test CecilExtensionMethodsTests.ImplementInterfaceDetectInheritedScenarios()

--- test/Starcounter.Weaver.Tests/CecilExtensionMethodsTests.cs
+++ test/Starcounter.Weaver.Tests/CecilExtensionMethodsTests.cs
@@ -1,5 +1,6 @@
 ﻿
 using Starcounter.Weaver.Tests.ExternalCode;
+using System;
 using System.Linq;
 using Xunit;
 
@@ -16,6 +17,16 @@ namespace Starcounter.Weaver.Tests {
     class SubClassThatInheritFieldAcrossAssembly : ClassDerivingExplicitStateReferenceFieldsBase {
 
     }
+
+    interface IInterfaceDerivingExternalInterface : IDisposable {
+    }
+
+    class ClassImplementingIInterfaceDerivingExternalInterface : IInterfaceDerivingExternalInterface {
+        void IDisposable.Dispose() {
+            throw new NotImplementedException();
+        }
+    }
+    class ClassDerivingClassImplementingIInterfaceDerivingExternalInterface : ClassImplementingIInterfaceDerivingExternalInterface { }
 
     public class CecilExtensionMethodsTests {
 
@@ -40,6 +51,22 @@ namespace Starcounter.Weaver.Tests {
             var module = TestUtilities.GetModuleOfCurrentAssembly();
             var oc = module.GetObjectConstructorReference();
             Assert.NotNull(oc);
+        }
+
+        [Fact]
+        public void ImplementInterfaceDetectInheritedScenarios() {
+            var module = TestUtilities.GetModuleOfCurrentAssembly();
+            
+            var disposable = module.GetTypeReferences().Single(r => r.FullName == typeof(IDisposable).FullName);
+            Assert.NotNull(disposable);
+
+            var type = module.Types.Single(t => t.FullName == typeof(ClassImplementingIInterfaceDerivingExternalInterface).FullName);
+            Assert.NotNull(type);
+            Assert.True(type.ImplementInterface(disposable));
+
+            type = module.Types.Single(t => t.FullName == typeof(ClassDerivingClassImplementingIInterfaceDerivingExternalInterface).FullName);
+            Assert.NotNull(type);
+            Assert.True(type.ImplementInterface(disposable));
         }
     }
 }
