9000bb651288bb91ca81913a357d51024a73f0ba
Per Samuelsson <per@starcounter.com>
2018-01-25 13:21:48 +0100

    New shared testing method - DefinitionOf() - to simply retrieve single definition from a given module

--- test/SharedTestUtilities/SharedTesting.cs
+++ test/SharedTestUtilities/SharedTesting.cs
@@ -1,7 +1,9 @@
 ﻿
 using Mono.Cecil;
 using Starcounter.Weaver;
+using System;
 using System.IO;
+using System.Linq;
 using Xunit;
 
 namespace SharedTestUtilities {
@@ -22,6 +24,15 @@ namespace SharedTestUtilities {
             var module = ModuleDefinition.ReadModule(new MemoryStream(testAssembly, false), readerParameters);
             Assert.NotNull(module);
             return module;
+        }
+
+        public static TypeDefinition DefinitionOf(this ModuleDefinition module, Type type) {
+            // Using First would be faster, but lets stick to Single as an
+            // extra testing measurement.
+            var def = module.Types.Single(t => t.MetadataToken.ToInt32() == type.MetadataToken);
+            Assert.NotNull(def);
+            return def;
+
         }
     }
 }
