c9617adb98cba96dc96afe8cd8f6fdb51263ff6c
Per Samuelsson <per@starcounter.com>
2018-01-25 16:12:45 +0100

    New TypeSpecializationComparer to help us resolve type hierarchies.

---
+++ src/Starcounter.Weaver/TypeSpecializationComparer.cs
@@ -0,0 +1,32 @@
+﻿using Mono.Cecil;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Starcounter.Weaver {
+
+    /// <summary>
+    /// Simple compararer that can be used to compare specialization level of
+    /// types.
+    /// </summary>
+    public class TypeSpecializationComparer : IComparer<TypeDefinition> {
+
+        public int Compare(TypeDefinition x, TypeDefinition y) {
+            Guard.NotNull(x, nameof(x));
+            Guard.NotNull(y, nameof(y));
+            
+            if (x == y || x.MetadataToken == y.MetadataToken) {
+                return 0;
+            }
+
+            if (x.IsAssignableFrom(y)) {
+                return -1;
+            }
+            else if (y.IsAssignableFrom(x)) {
+                return 1;
+            }
+
+            return 0;
+        }
+    }
+}
