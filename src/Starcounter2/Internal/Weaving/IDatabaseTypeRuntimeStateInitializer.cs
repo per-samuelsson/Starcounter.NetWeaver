09c837c84f097923875c172537ff482a0806bfde
Per Samuelsson <per@starcounter.com>
2018-01-30 15:34:14 +0100

    New static runtime database type initializer and an abstraction we route calls to, allowing an implementation to be injected.

---
+++ src/Starcounter2/Internal/Weaving/IDatabaseTypeRuntimeStateInitializer.cs
@@ -0,0 +1,10 @@
+﻿
+using System;
+
+namespace Starcounter2.Internal.Weaving {
+
+    public interface IDatabaseTypeRuntimeStateInitializer {
+
+        void InitializeType(Type weavedDatabaseType);
+    }
+}
---
+++ src/Starcounter2/Internal/Weaving/StaticRuntimeStateInitializer.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+
+namespace Starcounter2.Internal.Weaving {
+    
+    public static class StaticRuntimeStateInitializer {
+
+        internal static IDatabaseTypeRuntimeStateInitializer Current { get; set; }
+
+        public static void InitializeDatabaseType(Type weavedDatabaseType) {
+            // If no intializer is installed at the point of this call,
+            // thats an internal, and recognized, error.
+
+            Current.InitializeType(weavedDatabaseType);
+        }
+    }
+}
