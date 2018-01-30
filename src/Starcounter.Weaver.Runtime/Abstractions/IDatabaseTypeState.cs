281d994fa9c9c22c9b884c4cf5ac8323fb505ffc
Per Samuelsson <per@starcounter.com>
2018-01-30 13:49:37 +0100

    New IDatabaseTypeState abstraction, defined in Starcounter.Weaver.Runtime, allowing runtimes to access type-level state emitted to database classes by the weaver.

---
+++ src/Starcounter.Weaver.Runtime/Abstractions/IDatabaseTypeState.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Starcounter.Weaver.Runtime.Abstractions {
+
+    /// <summary>
+    /// Represent state for a given database type as governed by the
+    /// weaver. Used by hosts to initialize shared/static data of identified
+    /// database types.
+    /// </summary>
+    public interface IDatabaseTypeState {
+
+        ulong CreateHandle { get; set; }
+
+        ulong DeleteHandle { get; set; }
+
+        ulong GetPropertyHandle(string declaredPropertyName);
+
+        void SetPropertyHandle(string declaredPropertyName, ulong handle);
+    }
+}
