ba566be3430480f8b1caf7d0179dd17938d0e4f1
Per Samuelsson <per@starcounter.com>
2018-01-30 11:48:00 +0100

    New IDbProxyState interface in Starcounter2.

---
+++ src/Starcounter2/Internal/IDbProxyState.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Starcounter2.Internal {
+    /// <summary>
+    /// Provide what all routed interfaces will need in terms of state from
+    /// the pass-through type (typed as this). Weaver will explicitly implement
+    /// this and have knowledge on how to do that.
+    /// </summary>
+    public interface IDbProxyState {
+
+        ulong GetDbId();
+
+        void SetDbId(ulong id);
+
+        ulong GetDbRef();
+
+        void SetDbRef(ulong id);
+    }
+}
