0bbdb4c1a7a9c989ff331ddd27d08d9c99646368
Per Samuelsson <per@starcounter.com>
2018-01-30 15:34:59 +0100

    AssemblyRuntimeFacade now implement IAssemblyRuntimeFacade.RuntimeTypeStateInitializerMethod, returning our new static initializer entrypoint.

--- src/Starcounter2/Internal/Weaving/AssemblyRuntimeFacade.cs
+++ src/Starcounter2/Internal/Weaving/AssemblyRuntimeFacade.cs
@@ -26,7 +26,9 @@ namespace Starcounter2.Internal.Weaving {
         IEnumerable<RoutedInterfaceSpecification> IAssemblyRuntimeFacade.RoutedInterfaces => new RoutedInterfaceSpecification[0];
 
         Type IAssemblyRuntimeFacade.DbProxyStateInterfaceType => typeof(IDbProxyState);
-        
+
+        MethodInfo IAssemblyRuntimeFacade.RuntimeTypeStateInitializerMethod => typeof(StaticRuntimeStateInitializer).GetTypeInfo().GetMethod(nameof(StaticRuntimeStateInitializer.InitializeDatabaseType), BindingFlags.Public | BindingFlags.Static);
+
         MethodInfo IAssemblyRuntimeFacade.GetReadMethod(string type) {
             return crudProvider.GetReadMethod(type);
         }
