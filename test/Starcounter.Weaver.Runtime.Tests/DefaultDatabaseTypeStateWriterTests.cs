38bb6ad00a2d6a496b2a7bfaef078acffa611b46
Per Samuelsson <per@starcounter.com>
2018-01-30 13:50:51 +0100

    First set of unit tests for DefaultDatabaseTypeStateWriter.

---
+++ test/Starcounter.Weaver.Runtime.Tests/DefaultDatabaseTypeStateWriterTests.cs
@@ -0,0 +1,57 @@
+﻿
+using System;
+using Xunit;
+
+namespace Starcounter.Weaver.Runtime.Tests {
+
+    class TestTypeStateNames : DatabaseTypeStateNames {
+        readonly string createHandle = "createHandle";
+        readonly string deleteHandle = "deleteHandle";
+
+        public TestTypeStateNames(string customCreateHandle = null, string customDeleteHandle = null) {
+            createHandle = customCreateHandle ?? createHandle;
+            deleteHandle = customDeleteHandle ?? deleteHandle;
+        }
+        
+        public override string CreateHandle => createHandle;
+
+        public override string DeleteHandle => deleteHandle;
+
+        public override string GetPropertyHandleName(string propertyName) {
+            return "property_" + propertyName;
+        }
+    }
+
+    public class ClassWithCreateAndDeleteHandle {
+        protected static ulong createHandle = 0;
+        protected static ulong deleteHandle = 0;
+    }
+    
+    public class DefaultDatabaseTypeStateWriterTests {
+
+        [Fact]
+        public void NewWithNullTypeRaiseNullException() {
+            Assert.Throws<ArgumentNullException>(() => new DefaultDatabaseTypeStateWriter(null, new DatabaseTypeStateNames()));
+        }
+
+        [Fact]
+        public void NewWithNullNamesRaiseNullException() {
+            Assert.Throws<ArgumentNullException>(() => new DefaultDatabaseTypeStateWriter(GetType(), null));
+        }
+        
+        [Fact]
+        public void NewWithMissingFieldRaiseMeaningfulException() {
+            var type = typeof(ClassWithCreateAndDeleteHandle);
+
+            var names = new TestTypeStateNames(customCreateHandle: "missing_create");
+            var e = Assert.Throws<ArgumentException>(() => new DefaultDatabaseTypeStateWriter(type, names));
+            Assert.Contains(type.Name, e.Message);
+            Assert.Contains(names.CreateHandle, e.Message);
+
+            names = new TestTypeStateNames(customDeleteHandle: "missing_delete");
+            e = Assert.Throws<ArgumentException>(() => new DefaultDatabaseTypeStateWriter(type, names));
+            Assert.Contains(type.Name, e.Message);
+            Assert.Contains(names.DeleteHandle, e.Message);
+        }
+    }
+}
